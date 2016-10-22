d77c20bf1bd81c6edc4ee9f0411349f28a49fccc
Don Smith <locksmithdon@gmail.com>
2016-10-22 15:28:45 +1300

    display environment

---
+++ display-environment.js
@@ -0,0 +1,34 @@
+const blessed = require('blessed')
+
+module.exports = displayEnvironment
+
+const screen = blessed.screen()
+const box = blessed.box({
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  tags: true,
+  style: {
+    fg: 'white',
+    bg: 'black'
+  }
+})
+screen.append(box)
+
+screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
+
+function displayEnvironment (environment) {
+  const frame = createFrame(environment)
+  box.setContent(frame)
+  screen.render()
+}
+
+function createFrame (environment) {
+  let frame = ''
+  environment.forEach(row => {
+    const transformed = row.map(node => node ? '0' : ' ')
+    frame += transformed.join(' ') + '\n'
+  })
+  return frame
+}
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-
-// * create environment
-// * randomise envionment
-// display environment
+// * create a random environment
+// * display environment
 //
 // * create new environment
 //
 // * determine number of living neighbors
 // add new state of cell to new environment
-// repeat for next cell
+// * repeat for next cell
 //
-// display environment
+// * display environment
+
+const createEnvironment = require('./create-environment')
+const displayEnvironment = require('./display-environment')
+
+const random = true
+let environment = createEnvironment(30, random)
+
+displayEnvironment(environment)
+
+setInterval(() => {
+  environment = createEnvironment(30, random)
+  displayEnvironment(environment)
+}, 200)
